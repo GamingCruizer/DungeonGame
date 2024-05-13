a064706916246dc9f79c42508c7b19c7a9a40da5
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-05-13 08:38:38 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,9 +1,14 @@
 document.body.innerHTML += `<html>
   <!DOCTYPE html>
+  <html>
+  
   <head>
     <title>Whilly Wonka</title>
   </head>
+  
   <body>
     <h1>Whilly Wonka</h1>
   </body>
+  
+  </html>
 </html>`;
