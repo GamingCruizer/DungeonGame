8c1794df1161da81a9ed44d347e87346a6069bbf
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-05-16 08:09:22 -0700

    Update start.js

--- start.js
+++ start.js
@@ -9,13 +9,31 @@ document.body.innerHTML += `<html>
   </head>
   
   <body>
-  
+    <div id="menu">
+      <div id="menuItems">
+        <div id="rockMine">Rock</div>
+        <div id="woodMine">Wood</div>
+      </div>
+    </div>
   </body>
 
 
 
   <style>
-  
+    body {
+      background-color: powderblue;
+      text-align: center;
+    }
+
+    .menu {
+      top: 5px;
+      left: 5px;
+      padding: 10px;
+      height: auto:
+      width: 10%;
+      border: 1px solid black;
+      background-color: blue;
+    }
   </style>
   
   </html>
