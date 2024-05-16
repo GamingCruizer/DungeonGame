a34831f6a8892438a44649bc0618726e289e9e01
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-05-16 08:50:29 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 document.body.innerHTML += `<html>
-  <!DOCTYPE html>
-  <html lang="en">
+  
+ <!DOCTYPE html>
+ <html lang="en">
   
   <head>
     <meta charset="UFT-8">
@@ -10,9 +11,10 @@ document.body.innerHTML += `<html>
   
   <body>
     <div id="menu">
+      <div id="menuTitle">Money Mozilla</div>
       <div id="menuItems">
-        <div id="rockMine">Rock</div>
-        <div id="woodMine">Wood</div>
+        <div class="item" id="rockMine">Rock Mine</div>
+        <div class="item" id="woodMine">Wood Mine</div>
       </div>
     </div>
   </body>
@@ -21,23 +23,42 @@ document.body.innerHTML += `<html>
 
   <style>
     body {
-      background-color: powderblue;
+      background-color: #ADD8E6;
       text-align: center;
     }
 
+		/* Menu Stuff */
     #menu {
       position: absolute;
       top: 5px;
       left: 5px;
       padding: 10px;
-      height: 100%:
-      width: 10%;
+      height: 100%;
+      width: 20%;
+      overflow-x: hidden;
+      overflow-y: scroll;
+      border: 1px solid blue;
+      background-color: #7393B3;
+    }
+    #menuTitle {
+      width: 100%;
+      font-size: 120%;
+      border-bottom: 1px solid black;
+    }
+    #menuItems {
+   	  /* Nothing yet */
+    }
+    .item {
+      margin-top: 5px;
+      width: auto;
+      height: 5%;
+      background-color: white;
       border: 1px solid black;
-      background-color: blue;
     }
   </style>
   
-  </html>
+ </html>
+
 </html>`;
 
 
