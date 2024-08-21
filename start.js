da5f5163782c92eb698efe61b61a747b90621436
GamingCruizer <125952701+GamingCruizer@users.noreply.github.com>
2024-08-21 06:48:47 -0700

    Update start.js

--- start.js
+++ start.js
@@ -6,56 +6,18 @@ document.body.innerHTML += `<html>
   <head>
     <meta charset="UFT-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Money Mozilla</title>
+    <title>AI Chat with API Key</title>
   </head>
   
   <body>
-    <div id="menu">
-      <div id="menuTitle">Money Mozilla</div>
-      <div id="menuItems">
-        <div class="item" id="rockMine">Rock Mine</div>
-        <div class="item" id="woodMine">Wood Mine</div>
-      </div>
-    </div>
+    <h1>Talk to AI</h1>
+    <div>
+      <textarea id="userInput" rows="4" cols="50")
   </body>
 
 
 
-  <style>
-    body {
-      background-color: #ADD8E6;
-      text-align: center;
-    }
-
-		/* Menu Stuff */
-    #menu {
-      position: absolute;
-      top: 5px;
-      left: 5px;
-      padding: 10px;
-      height: 100%;
-      width: 20%;
-      overflow-x: hidden;
-      overflow-y: scroll;
-      border: 1px solid blue;
-      background-color: #7393B3;
-    }
-    #menuTitle {
-      width: 100%;
-      font-size: 120%;
-      border-bottom: 1px solid black;
-    }
-    #menuItems {
-   	  /* Nothing yet */
-    }
-    .item {
-      margin-top: 5px;
-      width: auto;
-      height: 5%;
-      background-color: white;
-      border: 1px solid black;
-    }
-  </style>
+ 
   
  </html>
 
